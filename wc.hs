d3781d4b5aff16b2219f1e4ca2e545b9881ab9b1
Walther Chen <walther.chen@gmail.com>
2015-02-11 22:08:40 -0500

    add basic command args parse

--- wc.hs
+++ wc.hs
@@ -4,6 +4,8 @@ import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
 import Data.Encoding
 import Data.Encoding.UTF8
+import System.Environment
+import System.Exit
 
 -- ByteString uses Word8 as base, not Char8 (truncates to one byte)
 -- but have to encode into utf8? This counts bytes, not char.
@@ -21,3 +23,16 @@ lineCount = length . lines
 
 maxLineLength :: String -> Int
 maxLineLength = maximum . map length . lines
+
+parse ["-h"] = usage >> exit
+parse ["-v"] = version >> exit
+parse []     = getContents
+parse fs     = concat `fmap` mapM readFile fs
+
+usage = putStrLn "Usage: wc [-vh] [file..]"
+version = putStrLn "wc in Haskell version 0.1"
+exit = exitWith ExitSuccess
+die = exitWith (ExitFailure 1)
+
+main = getArgs >>= parse >>= putStrLn . show . wordCount
+-- may need a library or monads to parse the different options.
